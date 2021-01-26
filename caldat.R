d503cfecac4b9cd5898a31b1fcd578e5b005125c
Pascal Yiou <pascal.yiou@lsce.ipsl.fr>
2021-01-26 15:16:24 +0100

    Update caldat.R

--- caldat.R
+++ caldat.R
@@ -32,3 +32,42 @@
   caldat <- list(month = mm, day = id, year = iyyy)
   invisible(caldat)
 }
+
+## The function computes Julian days from month, day, and year. 
+"julday" <- function (mm, id, iyyy)
+{
+  igreg <- 588829
+  mm <- trunc(mm)
+  id <- trunc(id)
+  iyyy <- trunc(iyyy)
+  im <- (iyyy == 0)
+  if (sum(im, na.rm = TRUE) > 0)
+    return("There is no year zero!")
+  if ((length(mm) != length(id)) | (length(mm) != length(iyyy)) |
+      (length(iyyy) != length(id)))
+    return("The vectors must have same length!")
+  im <- (iyyy < 0)
+  if (sum(im) > 0)
+    iyyy[im] <- iyyy[im] + 1
+  jy <- mm * 0
+  jm <- mm * 0
+  ja <- mm * 0
+  im <- (mm > 2)
+  if (sum(im) > 0) {
+    jy[im] <- iyyy[im]
+    jm[im] <- mm[im] + 1
+  }
+  im <- (mm <= 2)
+  if (sum(im) > 0) {
+    jy[im] <- iyyy[im] - 1
+    jm[im] <- mm[im] + 13
+  }
+  jul <- trunc(365.25 * jy) + trunc(30.6001 * jm) + id + 1720995
+  im <- (id + 31 * (mm + 12 * iyyy) >= igreg)
+  if (sum(im) > 0) {
+    ja[im] <- trunc(0.01 * jy)
+    jul[im] <- jul + 2 - ja[im] + trunc(0.25 * ja[im])
+  }
+  julday <- jul
+  invisible(julday)
+} 
